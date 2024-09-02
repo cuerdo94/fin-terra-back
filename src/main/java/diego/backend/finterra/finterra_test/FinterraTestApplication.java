0fcfaef2cc2860d105b8a095ccb2d14fe1865ab1
Diego R <diegosantisromero@gmail.com>
2024-09-02 13:46:45 -0400

    first commit

---
+++ src/main/java/diego/backend/finterra/finterra_test/FinterraTestApplication.java
@@ -0,0 +1,13 @@
+package diego.backend.finterra.finterra_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinterraTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinterraTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/diego/backend/finterra/finterra_test/FinterraTestApplicationTests.java
@@ -0,0 +1,13 @@
+package diego.backend.finterra.finterra_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinterraTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
